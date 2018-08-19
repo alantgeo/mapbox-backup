8a3e1d349fff6e5d968a016aa7a645b112ffe7af
Andrew Harvey <andrew@alantgeo.com.au>
2018-08-19 22:17:26 +1000

    output formatting

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
-var mkdirp = require('mkdirp');
-var async = require('async');
-var chalk = require('chalk');
-var error = chalk.bold.red;
+const fs = require('fs');
+const path = require('path');
+const mkdirp = require('mkdirp');
+const async = require('async');
+const chalk = require('chalk');
+const error = chalk.bold.red;
 
-var argv = require('yargs').argv
+const argv = require('yargs').argv
 
 // pretty print JSON outputs to make it easier to diff changes when committing backups into version control
-var json_spacing = 2;
+const json_spacing = 2;
 
-var backup = {};
+const backup = {};
 
 if (argv.all) {
     [
@@ -25,8 +26,8 @@ if (argv.all) {
         backup[backupOption]  = true;
     });
 } else {
-    for (var arg in argv) {
-        backup[arg] = argv[arg];;
+    for (const arg in argv) {
+        backup[arg] = argv[arg];
     }
 }
 
@@ -34,21 +35,21 @@ if (argv.all) {
 if (backup.styleObjects) backup.styleList = true;
 if (backup.datasetObjects) backup.datasetList = true;
 
-var accessToken = process.env.MapboxAccessToken;
+const accessToken = argv['access-token'] || process.env.MapboxAccessToken;
 
 if (accessToken === undefined) {
-    console.log(error("MapboxAccessToken environment variable missing"));
+    console.log(error("Either --access-token or MapboxAccessToken environment variable must be provided"));
     process.exit();
 }
 
-var MapboxClient = require('mapbox');
-var mapbox = new MapboxClient(accessToken);
+const MapboxClient = require('mapbox');
 
 // enable transparent pagination on the mapbox client
-var MapboxTransparentPagination = require('./mapbox-transparent-pagination');
-mapbox = MapboxTransparentPagination(mapbox);
+const MapboxTransparentPagination = require('./mapbox-transparent-pagination');
 
-var username = MapboxClient.getUser(accessToken);
+const mapbox = MapboxTransparentPagination(new MapboxClient(accessToken));
+
+const username = MapboxClient.getUser(accessToken);
 
 if (username === undefined) {
     console.log(error("Can't determine username from access_token"));
@@ -57,35 +58,51 @@ if (username === undefined) {
 
 console.log('Account Username:' + chalk.bold(username));
 
-mkdirp.sync(username);
+const outputPath = argv.output || username;
+
+mkdirp.sync(outputPath);
 
 async.series([
     function(callback) {
         if (backup.styleList) {
             mapbox.listAllStyles((err, styles) => {
-                console.log('Styles');
+                process.stdout.write('Styles List ');
                 if (err) {
+                    process.stdout.write(chalk.red('✖') + '\n');
                     console.error(err);
+                    if (backup.styleObjects) {
+                        process.stdout.write('Style Documents ' + chalk.yellow(' skipped') + '\n');
+                    }
                     callback(err);
                 }
-                fs.writeFileSync(username + '/' + 'styles.json', JSON.stringify(styles, null, json_spacing));
+                fs.writeFileSync(path.join(outputPath, 'styles.json'), JSON.stringify(styles, null, json_spacing));
+                process.stdout.write(chalk.green('✔') + '\n');
+
                 if (backup.styleObjects) {
-                    mkdirp.sync(username + '/styles');
+                    process.stdout.write('Style Documents ');
+                    mkdirp.sync(path.join(outputPath, 'styles'));
 
                     async.each(styles, function (styleObject, eachCallback) {
                         mapbox.readStyle(styleObject.id, (err, styleDoc) => {
                             if (err) {
-                                process.stdout.write(chalk.red('x'));
+                                process.stdout.write(chalk.red('✖'));
                                 console.error(err);
                                 eachCallback(err);
                             } else {
                                 process.stdout.write(chalk.green('.'));
-                                fs.writeFileSync(username + '/styles/' + styleObject.id + '.json', JSON.stringify(styleDoc, null, json_spacing));
+                                fs.writeFileSync(path.join(outputPath, 'styles', styleObject.id + '.json'), JSON.stringify(styleDoc, null, json_spacing));
                                 eachCallback(null);
                             }
                         });
                     }, function (err) {
-                        console.log(styles.length);
+                        const errors = err ? err.map((e) => { return !!e; }) : [];
+                        if (errors.length) {
+                            // some errors
+                            process.stdout.write(chalk.yellow('⚠') + ' ' + (styles.length - errors.length) + '/' + styles.length + '\n');
+                        } else {
+                            // no errors
+                            process.stdout.write(chalk.green('✔') + '\n');
+                        }
                         callback(err);
                     });
                 } else {
@@ -99,13 +116,14 @@ async.series([
     function(callback) {
         if (backup.tilesetList) {
             mapbox.listAllTilesets((err, tilesets) => {
-                console.log('Tilesets');
+                process.stdout.write('Tilesets List ');
                 if (err) {
+                    process.stdout.write(chalk.red('✖') + '\n');
                     console.error(err);
                     callback(err);
                 }
-                fs.writeFileSync(username + '/' + 'tilesets.json', JSON.stringify(tilesets, null, json_spacing));
-                console.log(tilesets.length);
+                fs.writeFileSync(path.join(outputPath, 'tilesets.json'), JSON.stringify(tilesets, null, json_spacing));
+                process.stdout.write(chalk.green('✔') + '\n');
                 callback(err);
             });
         } else {
@@ -115,29 +133,43 @@ async.series([
     function(callback) {
         if (backup.datasetList) {
             mapbox.listAllDatasets((err, datasets) => {
-                console.log('Datasets');
+                process.stdout.write('Datasets List ');
                 if (err) {
+                    process.stdout.write(chalk.red('✖') + '\n');
                     console.error(err);
+                    if (backup.datasetObjects) {
+                        process.stdout.write('Dataset Documents ' + chalk.yellow(' skipped') + '\n');
+                    }
                     callback(err);
                 }
-                fs.writeFileSync(username + '/' + 'datasets.json', JSON.stringify(datasets, null, json_spacing));
+                fs.writeFileSync(path.join(outputPath, 'datasets.json'), JSON.stringify(datasets, null, json_spacing));
+                process.stdout.write(chalk.green('✔') + '\n');
+
                 if (backup.datasetObjects) {
-                    mkdirp.sync(username + '/datasets');
+                    process.stdout.write('Dataset Documents ');
+                    mkdirp.sync(path.join(outputPath, 'datasets'));
 
                     async.eachLimit(datasets, 1, function (datasetObject, eachCallback) {
                         mapbox.listAllFeatures(datasetObject.id, {}, (err, collection) => {
                             if (err) {
-                                process.stdout.write(chalk.red('x'));
+                                process.stdout.write(chalk.red('✖'));
                                 console.error(err);
                                 eachCallback(err);
                             } else {
                                 process.stdout.write(chalk.green('.'));
-                                fs.writeFileSync(username + '/datasets/' + datasetObject.id + '.json', JSON.stringify(collection, null, json_spacing));
+                                fs.writeFileSync(path.join(outputPath, 'datasets', datasetObject.id + '.json'), JSON.stringify(collection, null, json_spacing));
                                 eachCallback(null);
                             }
                         });
                     }, function (err) {
-                        console.log(datasets.length);
+                        const errors = err ? err.map((e) => { return !!e; }) : [];
+                        if (errors.length) {
+                            // some errors
+                            process.stdout.write(chalk.yellow('⚠') + ' ' + (datasets.length - errors.length) + '/' + datasets.length + '\n');
+                        } else {
+                            // no errors
+                            process.stdout.write(chalk.green('✔') + '\n');
+                        }
                         callback(err);
                     });
                 } else {
@@ -151,13 +183,14 @@ async.series([
     function(callback) {
         if (backup.tokenList) {
             mapbox.listAllTokens((err, tokens) => {
-                console.log('Tokens');
+                process.stdout.write('Tokens ');
                 if (err) {
+                    process.stdout.write(chalk.red('✖') + '\n');
                     console.error(err);
                     callback(err);
                 }
-                fs.writeFileSync(username + '/' + 'tokens.json', JSON.stringify(tokens, null, json_spacing));
-                console.log(tokens.length);
+                fs.writeFileSync(path.join(outputPath, 'tokens.json'), JSON.stringify(tokens, null, json_spacing));
+                process.stdout.write(chalk.green('✔') + '\n');
                 callback(err);
             });
         } else {
@@ -168,8 +201,8 @@ async.series([
 function(err, results) {
     if (err) {
         console.error(err);
-        console.error(chalk.red('Backup failed'));
+        console.log(chalk.red('Backup failed'));
     } else {
-        console.error(chalk.green('Backup succeded'));
+        console.log(chalk.green('Backup succeded'));
     }
 });
